cb63921dbe061e215329106b54e98286fb9f6b21
Fernando Locatelli Maioli <fndmaioli5@gmail.com>
2019-08-14 17:12:14 -0300

    create project

---
+++ MovieDB_NanoChallenge/MovieDB_NanoChallenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieDB_NanoChallenge
+//
+//  Created by Fernando Locatelli Maioli on 14/08/19.
+//  Copyright © 2019 Fernando Locatelli Maioli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
